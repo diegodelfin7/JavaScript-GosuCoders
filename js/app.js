50198350f654ae965f9e12989cd393d4e128248e
diego delfin <diegodelfinacharte@gmail.com>
2015-01-07 00:24:01 -0500

    js limpio

--- js/app.js
+++ js/app.js
@@ -1,61 +1,116 @@
 
-function generarTabla(m,n){
+		function crearArray(m,n){
 
-	var contenedor = $("<div>");
-	//var numero[] = getRandomInt(0,8);
-	var numeros_elegidos = [];
-	numeros_elegidos = crearVectorTabla(m,n);
-	for(var i =0 ;i<numeros_elegidos.length;i++){
-		console.log(numeros_elegidos[i]);
-	}
-	/*
-	$("body").append(contenedor);
-	for(var i=0;i<m;i++){
-		for(var j=0;j<n;j++){
-				var logoCat = $("<div>");
-				logoCat.attr({class: 'border_tabla'});
-				logoCat.attr({ style: 'display:inline-block'});
+			var vector=[];
+			var cantidad = m*n;
 
-				logoCat.text(numeros_elegidos[m*n]);
-				contenedor.append(logoCat);
-				} 
-		contenedor.append('<br />');
-	}
-*/	
-}
+			for(var i=0 ; i < cantidad ; i++){
+				vector[i] = i+1;
+			}
+
+			vector = shuffle(vector);
+			vector = vector_conx(vector);
+
+			return vector;
+		}
+
+		function vector_conx(array){
+
+			var mayor = -1,j;
+			var count = array.length;
+			for (var i = 0; i < count; i++) {
+				if (array[i] > mayor) {
+					j=i;
+					mayor = array[i];
+				}
+			}
+			console.log('mayor de numeros',mayor);
+			array[j]= "X";
+			return array;
+		}
+
+		function shuffle(array){
+
+			 var currentIndex = array.length, temporaryValue, randomIndex ;
+
+			  // While there remain elements to shuffle...
+			  while (0 !== currentIndex) {
+
+			    // Pick a remaining element...
+			    randomIndex = Math.floor(Math.random() * currentIndex);
+			    currentIndex -= 1;
+
+			    // And swap it with the current element.
+			    temporaryValue = array[currentIndex];
+			    array[currentIndex] = array[randomIndex];
+			    array[randomIndex] = temporaryValue;
+			  }
+
+		  return array;
+		}
+
+		function crearTabla(array){
+
+			var tabla = '<table border="1">';
+			var tr ='';
+			var k =1;
+			for(var i=0; i<3 ; i++){
+				 tr += '<tr>';
+				for(var j=0; j<3 ;j++){
+		         	tr += '<td id="m'+array[array.length-k]+'">'+array[array.length-k]+'</td>';
+		         	k++;
+		     	}
+		     tr += '</tr>';
+			}
+
+			tabla += tr;
+			tabla += '</table>';
+
+			$("#lista").html(tabla);
+		}
 
 
-function crearVectorTabla(m,n){
-	var myArray = [];
-	var listaArray = [];
-
-	for(var i=0 ; i<(m*n)-1 ;i++){
-		myArray[i]=i+1;
-	}
-
-	listaArray = myArray.sort(function() {return Math.random() - 0.5});
-
-	listaArray[i] = "x";
-	return listaArray;
-}
+		function buscarPosX(array){
+			var posA = array.indexOf("X");
+			return posA;
+		}
 
 
-function crearTabla(){
+		$(document).keydown(function(event) {
+			var tmp; 
+			pos = buscarPosX(V);
+			if(event.which == 65){
+				tmp = V[pos];
+				V[pos]= V[pos+1];
+				V[pos+1] = tmp;
+				$("#lista").html("");
+				crearTabla(V);
+				//alert('a la izquierda');
+			}
 
-	var tabla = '<table border="1">';
-	var tr ='';
+			if(event.which == 68){
+				tmp = V[pos];
+				V[pos]= V[pos-1];
+				V[pos-1] = tmp;
+				$("#lista").html("");
+				crearTabla(V);
+				//alert('a la derecha');
+			}
 
-	for(var i=0; i<3 ; i++){
-		 tr += '<tr>';
-		for(var j=0; j<3 ;j++){
-         	tr += '<td>'+i+'</td>';
-     	}
-     tr += '</tr>';
-	}
+			if(event.which == 87){
+				alert('sube');
+			}
 
-	tabla += tr;
-	tabla += '</table>';
+			if(event.which == 83){
+				alert('baja');
+			}
+		});
 
-	$("#lista").html(tabla);
-}
+
+		var V =[];
+		var pos;
+		V = crearArray(3,3);
+		crearTabla(V);
+		console.log('Vector X : ',V);
+
 
